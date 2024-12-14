faa880a960e5028127f606bbb60566ccb25f66e1
selvaprasanth-sellasamy <prasanthsamy61@gmail.com>
2024-12-14 21:42:34 +0530

    Integration to nomad commenced

--- controllers/nomad/Blog.js
+++ controllers/nomad/Blog.js
@@ -21,13 +21,14 @@ const handleNewBlog = async(req,res) => {
     }
 }
 
-const getBlogs = async(_,res) => {
+const getContent = async(req,res) => {
     try{
         const con = await connect('nomad');
         if(!con.status){
             return res.status(500).json({response:"Couldn't establish connection to the database",error:con.error});
         }
-        const retrivedBlogs = await blogModel.find({},{"_id":0,"__v":0});
+        const id = await req.query.id;
+        const retrivedBlogs = await blogModel.findOne({_id:id},{"_id":0,"__v":0});
         return res.status(200).json({response:retrivedBlogs});
     }
     catch(error){
@@ -38,4 +39,30 @@ const getBlogs = async(_,res) => {
         await disConnect();
     }
 }
-module.exports = { handleNewBlog, getBlogs };
+
+const fetchAll = async(_,res) => {
+    const getImage = (content) => {
+        const imgRegex = /<img[^>]+src="([^">]+)"/i;
+        const match = content.match(imgRegex);
+        return match ? match[1] : null;
+    };
+    try{
+        const con = await connect('nomad');
+        if(!con.status){
+            return res.status(500).json({response:"Couldn't establish connection to the database",error:con.error});
+        }
+        let retrivedBlogs = await blogModel.find({},{title:1,content:1});
+        retrivedBlogs.forEach((blog)=>{
+            blog.content = getImage(blog.content);
+        })
+        return res.status(200).json({response:retrivedBlogs});
+    }
+    catch(error){
+        console.log(error);
+        return res.status(400).json({response:"Something went wrong",error:error});
+    }
+    finally{
+        await disConnect();
+    }
+}
+module.exports = { handleNewBlog, getContent, fetchAll };
--- models/nomad/Blog.js
+++ models/nomad/Blog.js
@@ -3,11 +3,19 @@ const BlogSchema = mongoose.Schema(
     {
         title:{
             type:String,
-            require:[true,"Blog title can't be empty"]
+            required:[true,"can't be empty"],
+            validate:{
+                validator: (value) => value.length > 4,
+                message: "Must be at least 5 characters long"
+            }
         },
         content:{
             type:String,
-            require:[true,"Blog content can't be empty"]
+            required:[true,"can't be empty"],
+            validate:{
+                validator: (value) => value.length > 5, //TODO: Validation should also include tags
+                message: "Must be at least 5 characters long"
+            }
         },
         published:{
             type:Boolean,
