5c3eee617fdb84c634bc538613ca3fbbd475a41f
selvaprasanth-sellasamy <prasanthsamy61@gmail.com>
2024-12-15 09:32:01 +0530

    Refactored exception handling

--- controllers/db.js
+++ controllers/db.js
@@ -1,22 +1,8 @@
 const mongoose = require('mongoose');
 const connect = async(dbName) => {
-    try{
-        await mongoose.connect(`mongodb://localhost:27017/${dbName}`);
-        return {status:true};
-    }
-    catch(error){
-        console.log(error);
-        return {staus:false,error:error};
-    }
+    await mongoose.connect(`mongodb://localhost:27017/${dbName}`);
 }
 const disConnect = async() => {
-    try{
-        await mongoose.disconnect();
-        return {status:true};
-    }
-    catch(error){
-        console.log(error);
-        return {staus:false,error:error};
-    }
+    await mongoose.disconnect();
 }
 module.exports = { connect, disConnect };
--- controllers/nomad/Blog.js
+++ controllers/nomad/Blog.js
@@ -1,68 +1,62 @@
 const blogModel = require("../../models/nomad/Blog");
 const { connect, disConnect } = require("../db");
-const handleNewBlog = async(req,res) => {
+
+const exceptionBound = async(statement,res) => {
     try{
-        const con = await connect('nomad');
-        if(!con.status){
-            return res.status(500).json({response:"Couldn't establish connection to the database",error:con.error});
+        await connect('nomad');
+        await statement();
+    }
+    catch(error){
+        console.error(error);
+        return res.status(400).json({response:"Something went wrong",error:error});
+    }
+    finally{
+        try{
+            await disConnect();
         }
+        catch(error){
+            console.warn(`Error while disconnecting from the database ${error}`);
+        }
+    }
+}
+
+const handleNewBlog = (req,res) => {
+    exceptionBound(async() => {
         const createBlog = await blogModel.create(req.body);
         if(!createBlog){
             return res.status(400).json({response:"Couldn't save blog into database"});
         }
-        return res.status(200).json({response:"Blog saved"});
-    }
-    catch(error){
-        console.log(error);
-        return res.status(400).json({response:"Something went wrong",error:error});
-    }
-    finally{
-        await disConnect();
-    }
+        return res.status(201).json({response:"Blog saved"});
+    },res);
 }
 
-const getContent = async(req,res) => {
-    try{
-        const con = await connect('nomad');
-        if(!con.status){
-            return res.status(500).json({response:"Couldn't establish connection to the database",error:con.error});
-        }
+const getContent = (req,res) => {
+    exceptionBound(async() => {
         const id = await req.query.id;
         const retrivedBlogs = await blogModel.findOne({_id:id},{"_id":0,"__v":0});
+        if(retrivedBlogs.length < 1){
+            return res.status(404).send();
+        }
         return res.status(200).json({response:retrivedBlogs});
-    }
-    catch(error){
-        console.log(error);
-        return res.status(400).json({response:"Something went wrong",error:error});
-    }
-    finally{
-        await disConnect();
-    }
+    },res);
 }
 
-const fetchAll = async(_,res) => {
-    const getImage = (content) => {
-        const imgRegex = /<img[^>]+src="([^">]+)"/i;
-        const match = content.match(imgRegex);
-        return match ? match[1] : null;
-    };
-    try{
-        const con = await connect('nomad');
-        if(!con.status){
-            return res.status(500).json({response:"Couldn't establish connection to the database",error:con.error});
-        }
+const fetchAll = (_,res) => {
+    exceptionBound(async() => {
+        const getImage = (content) => {
+            const imgRegex = /<img[^>]+src="([^">]+)"/i;
+            const match = content.match(imgRegex);
+            return match ? match[1] : null;
+        };
         let retrivedBlogs = await blogModel.find({},{title:1,content:1});
+        if(retrivedBlogs.length < 1){
+            return res.status(404).send();
+        }
         retrivedBlogs.forEach((blog)=>{
             blog.content = getImage(blog.content);
         })
         return res.status(200).json({response:retrivedBlogs});
-    }
-    catch(error){
-        console.log(error);
-        return res.status(400).json({response:"Something went wrong",error:error});
-    }
-    finally{
-        await disConnect();
-    }
+    },res);
 }
+
 module.exports = { handleNewBlog, getContent, fetchAll };
