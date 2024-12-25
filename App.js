20b36d7abf6be2e6bef503d13c8dde9a7bd899c4
selvaprasanth-sellasamy <prasanthsamy61@gmail.com>
2024-12-25 09:02:18 +0530

    Changes in routes

--- App.js
+++ App.js
@@ -3,14 +3,14 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyparser = require('body-parser');
 const routes = require('./routes/portfolio/Email');
-const nomadBlogRoute = require('./routes/nomad/Blog');
+const nomadBlog = require('./routes/nomad/Blog');
 dotenv.config();
 const App = express();
 App.use(bodyparser.json({limit:'50mb'}));
 App.use(cors());
 App.use(express.json());
 App.use('/portfolio',routes);
-App.use('/nomad',nomadBlogRoute);
+App.use('/nomad/blog',nomadBlog);
 App.use(express.static('views'));
 const port = process.env.PORT;
 App.listen(port,()=>{
--- routes/nomad/Blog.js
+++ routes/nomad/Blog.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { handleNewBlog, fetchAll, getContent, deleteBlog } = require("../../controllers/nomad/Blog");
-router.post('/addblog',handleNewBlog);
-router.get('/fetchblogs',fetchAll);
+const { handleNewBlog, fetchAll, getContent, deleteBlog, updateBlog } = require("../../controllers/nomad/Blog");
+router.post('/add',handleNewBlog);
+router.get('/fetch',fetchAll);
 router.get('/getcontent',getContent);
-router.delete('/deleteBlog',deleteBlog);
+router.delete('/delete',deleteBlog);
+router.patch('/update',updateBlog);
 module.exports = router;
