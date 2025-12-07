4f15b3b8a05197b899e4a027171fda2a76236009
Selvaprasanth Sellasamy <prasanthsamy61@gmail.com>
2025-12-07 10:45:54 +0530

    Bug fix

--- controllers/db.js
+++ controllers/db.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
+const connections = {};
 
 const getConnection = (dbName) => {
-    const connections = {};
     if(connections[dbName]) return connections[dbName];
     try{
         const url = `${process.env.MONGO_DB_BASE_URL}${dbName}`;
+        // const userName = encodeURIComponent(process.env.MONGODB_USERNAME);
+        // const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+        // const url = `mongodb+srv://${userName}:${password}@cluster0.fjloxgx.mongodb.net/${dbName}?appName=Cluster0`;
         const con = mongoose.createConnection(url,{
             maxPoolSize: 10,
             minPoolSize: 1
@@ -16,5 +19,11 @@ const getConnection = (dbName) => {
        console.error("Error while connecting the database\n",error);
     }
 }
+
+process.on("SIGINT",async() => {
+    await mongoose.disconnect();
+    console.log("Disconnected from the MongoDB databases.");
+    process.exit(0);
+})
 
 module.exports = { getConnection };
